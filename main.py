655ee5c02824e75e120c017d52c10b159e33072f
Oleksandr <alex.rozin@stud.onu.edu.ua>
2023-10-05 00:24:07 +0300

    Add Function and Plotter Settings

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from controller import Controller
 
 
 settings = {
+    "Function Settings": {
+        "Function": "(x_1**2 + x_2 - 11)**2 + (x_1 + x_2**2 - 7)**2",
+        "Starting Coordinates": [1, 1],
+        "Accuracy": -5,
+        "Specified Minimum Coordinates": [3, 2]
+    },
+
     "Methods Selection": {
         "Conjugate Gradients": True,
         "Conjugate Gradients 1st Modification": True,
@@ -12,6 +19,10 @@ settings = {
     "Alpha k Selection": {
         "Single-Factor Minimization": True,
         "Doubling Method": False
+    },
+
+    "Plotter Settings": {
+        "Plot": True
     }
 }
 
